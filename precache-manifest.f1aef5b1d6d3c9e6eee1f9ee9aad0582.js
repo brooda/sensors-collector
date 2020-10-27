1fb182cd6515471982399fe586e9b241b417493c
Piotr <brodkap@student.mini.pw.edu.pl>
2020-10-27 10:33:47 +0100

    Updates

---
+++ precache-manifest.f1aef5b1d6d3c9e6eee1f9ee9aad0582.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee42de7faa30aca753f9770cf87ad06",
+    "url": "/sensors-collector/index.html"
+  },
+  {
+    "revision": "70d703135187826926ad",
+    "url": "/sensors-collector/static/css/2.bcb0d36b.chunk.css"
+  },
+  {
+    "revision": "6922a9faf652ba7f85f3",
+    "url": "/sensors-collector/static/css/main.5e33e3a7.chunk.css"
+  },
+  {
+    "revision": "70d703135187826926ad",
+    "url": "/sensors-collector/static/js/2.a13c790a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sensors-collector/static/js/2.a13c790a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6922a9faf652ba7f85f3",
+    "url": "/sensors-collector/static/js/main.f977d7a9.chunk.js"
+  },
+  {
+    "revision": "e3917185c8b1b70997ba",
+    "url": "/sensors-collector/static/js/runtime-main.4eb23adf.js"
+  }
+]);
